06f1a571621d598ba9df133b486b5a3e7806ff34
luckhee <dcs1206@naver.com>
2025-08-11 16:04:34 +0900

    5단계

--- src/main/kotlin/WiseSaying.kt
+++ src/main/kotlin/WiseSaying.kt
@@ -1,34 +1,37 @@
 fun main() {
     println("== 명언 앱 ==")
     var id = 0
-
-
-
-
-
-    while(true) {
+    val sayings = mutableListOf<WiseSaying>()
+    while (true) {
         println("명령)")
-        var input = readLine()
-        if(input=="등록"){
+        var input = readlnOrNull()!!.trim()
+        if (input == "등록") {
 
             println("명언 : ")
-            var wiseSaying = readLine()
+            var wiseSaying = readlnOrNull()!!.trim()
             println("명언 : $wiseSaying")
 
             println("작가 : ")
-            var author = readLine()
+            var author = readlnOrNull()!!.trim()
             println("작가: $author")
-            id+=1
+            id += 1
+
+            val newSaying = WiseSaying(id, author, wiseSaying)
+            sayings.add(newSaying)
 
             println("$id 번 명언이 등록되었습니다.")
-        } else if(input =="종료") {
+        } else if (input == "종료") {
             println("명령) 종료")
             break
+        } else if (input == "목록") {
+            println("번호 / 작가 / 명언")
+            println("----------------------")
+            for(saying in sayings.reversed()) {
+                println("${saying.id} / ${saying.author} / ${saying.wiseSaying}")
+            }
         }
-
-
     }
-
+}
+data class WiseSaying(var id: Int , var author: String, var wiseSaying:String){
 
 }
-
