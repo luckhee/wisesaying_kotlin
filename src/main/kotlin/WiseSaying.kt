6e73485bfa596e83c2ed6f112ff451be02eb95d2
luckhee <dcs1206@naver.com>
2025-08-11 15:35:47 +0900

    4단계

--- src/main/kotlin/WiseSaying.kt
+++ src/main/kotlin/WiseSaying.kt
@@ -1,12 +1,34 @@
+fun main() {
+    println("== 명언 앱 ==")
+    var id = 0
 
-    fun main() {
-        var id = 1
 
-        println("== 명언 앱 ==")
-        println("명언) 등록")
-        println("명언 : 현재를 사랑하라.")
-        println("작가: 작자미상")
-        println("$id 번 명언이 등록되었습니다.")
-        println("명령) 종료")
+
+
+
+    while(true) {
+        println("명령)")
+        var input = readLine()
+        if(input=="등록"){
+
+            println("명언 : ")
+            var wiseSaying = readLine()
+            println("명언 : $wiseSaying")
+
+            println("작가 : ")
+            var author = readLine()
+            println("작가: $author")
+            id+=1
+
+            println("$id 번 명언이 등록되었습니다.")
+        } else if(input =="종료") {
+            println("명령) 종료")
+            break
+        }
+
+
     }
+
+
+}
 
