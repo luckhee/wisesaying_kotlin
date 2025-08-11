54017b7c624f652704032b9b9add1c1e6b45fba3
luckhee <dcs1206@naver.com>
2025-08-11 15:19:45 +0900

    3단계

--- src/main/kotlin/WiseSaying.kt
+++ src/main/kotlin/WiseSaying.kt
@@ -1,9 +1,12 @@
 
     fun main() {
+        var id = 1
+
         println("== 명언 앱 ==")
         println("명언) 등록")
         println("명언 : 현재를 사랑하라.")
         println("작가: 작자미상")
+        println("$id 번 명언이 등록되었습니다.")
         println("명령) 종료")
     }
 
