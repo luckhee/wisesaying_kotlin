523affa79ff8c808f681750fa2840a0f3f5b427f
luckhee <dcs1206@naver.com>
2025-08-11 15:18:07 +0900

    2단계

--- src/main/kotlin/WiseSaying.kt
+++ src/main/kotlin/WiseSaying.kt
@@ -1,6 +1,9 @@
 
     fun main() {
         println("== 명언 앱 ==")
-        println("명언) 종료")
+        println("명언) 등록")
+        println("명언 : 현재를 사랑하라.")
+        println("작가: 작자미상")
+        println("명령) 종료")
     }
 
